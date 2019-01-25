ec83dcdc80065b42e6fa40be47e04c5503777aa7
ryan m <contact@ryanmeek.com>
2019-01-25 18:46:12 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,99 @@
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+  ]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  turns = 0
+  board.each do |board|
+    if board == "X" || board == "O"
+      turns += 1
+    end
+  end
+  return turns
+end
+
+def current_player(board)
+  player_turn = turn_count(board).even? ? "X" : "O"
+  return player_turn
+end
+
+def won?(board)
+  ["X", "O"].each do |player|
+    WIN_COMBINATIONS.each do |combo|
+      if combo.all?{|combo| board[combo] == player}
+        return combo
+      end
+    end
+  end
+  return nil
+end
+
+def full?(board)
+  board.all?{|board| board == "X" || board == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  won?(board) ? board[won?(board)[0]] : nil
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
